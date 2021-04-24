5aac772100d0d0ed5ffecc532cb230b512997a09
Aaron McKie <aaron.mckie@icloud.com>
2021-04-24 17:52:48 +0100

    models code created

---
+++ models/gymclass.py
@@ -0,0 +1,7 @@
+class gymClass:
+    def __init__(self, lesson_name, duration, difficulty_level, capacity, id=None):
+        self.lesson_name = lesson_name
+        self.duration = duration
+        self.difficulty_level = difficulty_level
+        self.capacity = capacity
+        self.id = id
---
+++ models/schedule.py
@@ -0,0 +1,5 @@
+class Schedule:
+    def __init__(self, member, gymclass, id=None):
+        self.member = member
+        self.gymclass = gymclass
+        self.id = id
