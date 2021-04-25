81436a4df0e12d8dc269a06facbd27548d81c0ac
Aaron McKie <aaron.mckie@icloud.com>
2021-04-25 14:50:12 +0100

    some more controller code added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 
+from controllers.members_controller import members_blueprint
+from controllers.gymclass_controller import gymclass_blueprint
+
 app = Flask(__name__)
 
 app.register_blueprint(gynclasses_blueprint)
--- controllers/gymclass_controller.py
+++ controllers/gymclass_controller.py
@@ -1,1 +1,4 @@
 from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.gymclass import gymClass
+import gymclass_repository as gymclass_repository
