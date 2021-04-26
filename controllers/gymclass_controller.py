a935faadf7ba8977d12c569cc89db8b337a9eaeb
Aaron McKie <aaron.mckie@icloud.com>
2021-04-26 10:51:35 +0100

    gymclass route added in controller

--- controllers/gymclass_controller.py
+++ controllers/gymclass_controller.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, Flask, redirect, render_template, request
-
+from flask import Flask, redirect, render_template, request
+from flask import Blueprint
 from models.gymclass import gymClass
 import repositories.gymclass_repository as gymclass_repository
+
+gymclasses_blueprint = Blueprint("gymclasses", __name__)
+
+@gymclasses_blueprint.route("/gynclasses")
+def gymclasses():
+    gymclasses = gymclass_repository.select_all()
+    return render_template("gymclasses/index.html", gymclasses = gymclasses)
