1c9766b93244c3e966e6c068e0b6882939bfb6fd
Aaron McKie <aaron.mckie@icloud.com>
2021-04-26 16:23:49 +0100

    404 when trying to access /gymclasses, attempting fix

--- console.py
+++ console.py
@@ -16,23 +16,27 @@ member1 = Member('Eddie Hall', 'Elite')
 member_repository.save(member1)
 
 member2 = Member('Not Jim', 'Intermediate')
+member_repository.save(member2)
 
 member3 = Member('Jo Smith', 'Beginner')
+member_repository.save(member3)
 
 gymclass1 = gymClass('Strongman Prep', 60, 'Advanced', 10)
 gymclass_repository.save(gymclass1)
 
 gymclass2 = gymClass('Zumba Intro', 30, 'Beginner', 20)
+gymclass_repository.save(gymclass2)
 
 gymclass3 = gymClass('HIIT Intermediate', 15, 'Intermediate', 25)
+gymclass_repository.save(gymclass3)
 
 schedule1 = Schedule(member1, gymclass1)
 schedule_repository.save(schedule1)
 
 schedule2 = Schedule(member2, gymclass3)
+schedule_repository.save(schedule2)
 
 schedule3 = Schedule(member3, gymclass2)
-
-schedule4 = Schedule(member, gymclass)
+schedule_repository.save(schedule3)
 
 pdb.set_trace()
