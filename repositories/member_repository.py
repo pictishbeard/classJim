a55711ec33eb0568fa878c212511b2e1548fdfa7
Aaron McKie <aaron.mckie@icloud.com>
2021-04-26 11:47:50 +0100

    fsome functional code written prior to testing

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -10,3 +10,32 @@ def save(member):
     member.id = results[0]['id']
     return member
 
+def select_all():
+    members = []
+
+    sql = "SELECT * FROM members"
+    results = run_sql(sql)
+    for row in results:
+        member = Member(row['full_name'], row['experience_level'], row['id'])
+        members.append(member)
+    return members
+
+def select(id):
+    member = None
+    sql = "SELECT * FROM members WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+def class_bookings(member):
+    class_bookings = []
+
+    sql = "SELECT class_bookings.* FROM class_bookings INNER JOIN gym_classes ON gym_classes.gymclass_id = gymclasses.id WHERE member_id = %s"
+    values = [member.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        class_booking = gymClass(row['lesson_name'], row['duration'], row['difficulty_level'], row['capacity'])
+        class_bookings.append(class_booking)
+
+    return class_bookings
+
---
+++ repositories/schedule_repository.py
@@ -0,0 +1,7 @@
+from db.run_sql import run_sql
+
+from models.schedule import Schedule
+from models.gymclass import gymClass
+from models.member import Member
+import repositories.member_repository as member_repository
+import repositories.gymclass_repository as gymclass_repository
