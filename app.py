36b1e6672b5217b5ae3976875109df51ead9946f
Aaron McKie <aaron.mckie@icloud.com>
2021-04-26 11:08:25 +0100

    Homepage up and running

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
 from controllers.members_controller import members_blueprint
-from controllers.gymclass_controller import gymclass_blueprint
+from controllers.gymclass_controller import gymclasses_blueprint
 from controllers.schedule_controller import schedules_blueprint
 
 app = Flask(__name__)
 
-app.register_blueprint(gynclasses_blueprint)
+app.register_blueprint(gymclasses_blueprint)
 app.register_blueprint(members_blueprint)
 app.register_blueprint(schedules_blueprint)
 
--- controllers/schedule_controller.py
+++ controllers/schedule_controller.py
@@ -1,1 +1,12 @@
-from flask import Blueprint, Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request
+from flask import Blueprint
+from models.schedule import Schedule
+import repositories.schedule_repository as schedule_repository
+import repositories.member_repository as member_repository
+import repositories.gymclass_repository as gymclass_repository
+
+schedules_blueprint = Blueprint("schedules", __name__)
+@schedules_blueprint.route("/schedules")
+def schedules():
+    schedules = schedule_repository.select_all()
+    return render_template("schedules/index.html", schedules = schedules)
