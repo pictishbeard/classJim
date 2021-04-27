98cdc21a732c8e3667b9051abee954837c4419cb
Aaron McKie <aaron.mckie@icloud.com>
2021-04-27 10:08:38 +0100

    removed unused comments from members_controller

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -5,23 +5,15 @@ import repositories.member_repository as member_repository
 
 members_blueprint = Blueprint("members", __name__)
 
-#index route
+#index route for members
 @members_blueprint.route("/members")
 def members():
     members = member_repository.select_all()
     return render_template("members/index.html", members=members)
 
-#update route
+#show member info route
 @members_blueprint.route("/members/<id>")
 def show_members(id):
     member = member_repository.select(id)
     gymclasses = member_repository.gymclasses(member)
     return render_template("members/show.html", member=member, gymclasses=gymclasses)
-
-#create route
-
-#new route
-
-#edit route
-
-#delete route
