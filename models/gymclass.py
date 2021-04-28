13b27e35ee19e84ac9cf1cab036b92abfcd39089
Aaron McKie <aaron.mckie@icloud.com>
2021-04-28 11:53:40 +0100

    Added extra param to class

--- models/gymclass.py
+++ models/gymclass.py
@@ -1,7 +1,8 @@
 class gymClass:
-    def __init__(self, lesson_name, duration, difficulty_level, capacity, id = None):
+    def __init__(self, lesson_name, duration, difficulty_level, capacity, day, id = None):
         self.lesson_name = lesson_name
         self.duration = duration
         self.difficulty_level = difficulty_level
         self.capacity = capacity
+        self.day = day
         self.id = id
