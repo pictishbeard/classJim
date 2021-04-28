ec91156c283b954f7c7cdaed72e1c06a2a67f74e
Aaron McKie <aaron.mckie@icloud.com>
2021-04-28 12:13:16 +0100

    added more functions to gymclass repo

--- repositories/gymclass_repository.py
+++ repositories/gymclass_repository.py
@@ -6,20 +6,20 @@ from models.gymclass import gymClass
 from models.member import Member
 
 def save(gymclass):
-    sql = "INSERT INTO gymclasses(lesson_name, duration, difficulty_level, capacity) VALUES ( %s, %s, %s, %s ) RETURNING id"
-    values = [gymclass.lesson_name, gymclass.duration, gymclass.difficulty_level, gymclass.capacity]
+    sql = "INSERT INTO gymclasses(lesson_name, duration, difficulty_level, capacity, day) VALUES ( %s, %s, %s, %s, %s ) RETURNING id"
+    values = [gymclass.lesson_name, gymclass.duration, gymclass.difficulty_level, gymclass.capacity, gymclass.day]
     results = run_sql( sql, values )
     gymclass.id = results[0]['id']
-    return gymclass
+    gymclass.id = id(o)
+    return f"Notification: {gymclass.lesson_name} on {gymclass.day} has successfully saved"
 
 def select_all():
     gymclasses = []
-
     sql = "SELECT * FROM gymclasses"
     results = run_sql(sql)
 
     for row in results:
-        gymclass = gymClass(row['lesson_name'], row['duration'], row['difficulty_level'], row['capacity'], row['id'])
+        gymclass = gymClass(row['lesson_name'], row['duration'], row['difficulty_level'], row['capacity'], row['day'], row['id'])
         gymclasses.append(gymclass)
     return gymclasses
 
@@ -30,12 +30,11 @@ def select(id):
     result = run_sql(sql, values)[0]
 
     if result is not None:
-        gymclass = gymClass(result['lesson_name'], result['duration'], result['difficulty_level'], result['capacity'], result['id'])
+        gymclass = gymClass(result['lesson_name'], result['duration'], result['difficulty_level'], result['capacity'], result['day'], result['id'])
     return gymclass
 
-def members(gymclass):
+def members_in_class(gymclass):
     members = []
-
     sql = "SELECT members.* FROM members INNER JOIN schedules ON schedules.member_id = members.id WHERE gymclass_id = %s"
     values = [gymclass.id]
     results = run_sql(sql, values)
@@ -43,9 +42,18 @@ def members(gymclass):
     for row in results:
         member = Member(row['full_name'], row['experience_level'])
         members.append(member)
-    
     return members
+
+def delete(id):
+    sql = "DELETE FROM gymclasses WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
 
 def delete_all():
     sql = "DELETE FROM gymclasses"
     run_sql(sql)
+
+def update(gymclass):
+    sql = "UPDATE gymclasses SET (lesson_name, duration, difficulty_level, capacity, day) = (%s, %s, %s, %s, %s) WHERE id = %s"
+    values = [gymclass.lesson_name, gymclass.duration, gymclass.difficulty_level, gymclass.capacity, gymclass.day]
+    run_sql(sql, values)
